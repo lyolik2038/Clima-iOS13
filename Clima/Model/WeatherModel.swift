50e595216a35d738b236dd21475c3ccf91acf971
Valeriia Tishina <lyolik2038@gmail.com>
2021-01-28 01:46:48 +0400

    added location

--- Clima/Model/WeatherModel.swift
+++ Clima/Model/WeatherModel.swift
@@ -14,7 +14,7 @@ struct WeatherModel {
     let temperature: Double
     
     var temperatureString: String {
-        return String(format: "%.1f", temperature)
+        return String(format: "%.0f", temperature)
     }
     
     var conditionName: String {
